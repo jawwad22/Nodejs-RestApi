147e0472bdf4987bd6f875080e321b0f9b63f96e
unknown <mjawwadiqbal22@gmail.com>
2019-03-30 14:20:26 -0700

    --person with query

--- src/routes/person.js
+++ src/routes/person.js
@@ -1,8 +1,21 @@
 let express =require('express')
 let router=express.Router();
 
+//QuerySting =
 router.get('/person',(req,res)=>{
-    res.send('You are at person')
+    if(req.query.name){
+        res.send(`You are at person ${req.query.name}`)
+    }
+    else {
+        res.send('You have requested a person')
+    }
+})
+
+
+//param property on the request objec
+router.get('/person/:name',(req,res)=>{
+
+    res.send(`You are at person  ${req.param.name}`)
 })
 
 module.exports=router
