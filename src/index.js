7acf3d486444d6dc2e6f70f1f62aeb06a58e2a90
unknown <mjawwadiqbal22@gmail.com>
2019-03-30 14:25:54 -0700

    --Middleware added

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ let app =express();
 
 let personRoute=require('./routes/person')
 
+app.use((req,res,next)=>{
+    console.log(`${new Data().toString()}=>${req.originalUrl}`)
+    next();
+})
 app.use(personRoute);
 app.use(express.static('public'))
+
+
 
 const PORT=process.env.PORT || 3000;
 app.listen(PORT,()=>{
